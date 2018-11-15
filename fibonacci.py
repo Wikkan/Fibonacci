fc846d6f03bc0b48eeb792833be558e0eecbd32b
Josué <josuji-alfa@hotmail.com>
2018-11-14 21:49:33 -0600

    Comandos para heroku

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,13 @@
-def fib(n):
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def fib():
+    n = 10
+    
     a = 0
     b = 1
-    i = 0
      
     for x in range(n):
         a, b = b, a+b
